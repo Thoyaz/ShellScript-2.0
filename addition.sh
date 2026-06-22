c984d05189f6e32a0aa17eea133a5567fcf53c48
Thoyaz <thoyazthoyazpc@gmail.com>
2026-06-22 06:19:17 +0530

    variables

--- addition.sh
+++ addition.sh
@@ -3,9 +3,9 @@
 AGE1=25
 ANGE2="Thoyaz"
 
-echo "Total age is : $((ANGE1+AGE2))"
+echo "Total age is : $(($ANGE1+$AGE2))"
 
 AGE3=25
 ANGE4=5
 
-echo "Total age is : $((ANGE3+AGE4))"
+echo "Total age is : $(($ANGE3+$AGE4))"
