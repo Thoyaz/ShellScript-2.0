f5cbf796a41d0e1dcfd887dfe5320b0f957c13f2
Thoyaz <thoyazthoyazpc@gmail.com>
2026-06-22 07:11:20 +0530

    variables

---
+++ using-functionsy.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+USER_INFO=$(id -u)
+
+#verify user
+if [[ $USER_INFO -ne 0 ]]; then
+    echo "Please run the script as root user"
+    exit 1
+fi
+
+VALIDATE(){
+    if [[ $1 -ne 0 ]]; then
+        echo "$2 : FAILED"
+        exit 1
+    else
+        echo "$2 : SUCCESS"
+    fi 
+}
+
+# Install nginx
+echo "Installing nginx..."
+sudo apt install -y nginx
+
+VALIDATE $? "Nginx installation"
+
+#install mysql
+echo "Installing nginx..."
+sudo apt install -y mysql-server
+
+VALIDATE $? "Mysql installation"
+
+#install nodejs
+echo "Installing nginx..."
+sudo apt install -y nodejs npm
+
+VALIDATE $? "nodejs installation"
