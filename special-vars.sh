03273f25f5dd147b97507116e2152c38201a0e8e
Thoyaz <thoyazthoyazpc@gmail.com>
2026-06-22 06:10:38 +0530

    variables

---
+++ special-vars.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+NAME=$1
+echo "name comming frmo doller-1 : ${NAME}"
+
+SKILLS=$@
+echo "skills comming from doller-@: ${SKILLS}"
+
+echo "Number of args passed to the script: $#"
+
+echo "Scrit name : $0"
+
+echo "Precent working directory is : $PWD"
+
+echo "user name of the person executing the script is : $USER"
+
+echo "Home directory of the person executing the script is : $HOME"
+
+echo "PID of the script is : $$"
+
+echo "background process id is : $!"
+
+echo "exit status of the last command executed is : $?"
