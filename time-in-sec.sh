15108b5b2e086453d8ee626f038221366710383b
Thoyaz <thoyazthoyazpc@gmail.com>
2026-06-22 06:03:30 +0530

    variables

--- time-in-sec.sh
+++ time-in-sec.sh
@@ -5,5 +5,5 @@ sleep 10
 
 END_TIME=$(date +%s)
 echo "The end time of the script is: ${END_TIME}"
-TOTAL_TIME=$((END_TIME-START_TIME))
+TOTAL_TIME=$(($END_TIME-$START_TIME))
 echo "The total time taken to execute the script is: ${TOTAL_TIME} seconds"
