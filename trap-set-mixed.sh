0970595e34cc75bd3afb704e1c5474ea2892f664
Thoyaz <thoyazthoyazpc@gmail.com>
2026-06-22 08:42:16 +0530

    variables

--- trap-set-mixed.sh
+++ trap-set-mixed.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 Set -e # Set will consider errors as ERR
-Trap ‘echo “There is an error in $LINENO, command: $BASH_COMMAND”’ ERR’
+Trap 'echo “There is an error in $LINENO, command: $BASH_COMMAND”' ERR
 
 echo "hello world"
 echo "This is a test script to demonstrate the use of trap command in bash scripting"
