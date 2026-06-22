9c70cc0a356b877dad51f62b27fd774aa8c0745d
Thoyaz <thoyazthoyazpc@gmail.com>
2026-06-22 05:45:26 +0530

    variables

---
+++ variables-conversation.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1="Thoyaz"
+PERSON2="PC"
+
+echo "${PERSON1}: Hello ${PERSON2}"
+echo "${PERSON2}: Hello ${PERSON1}"
+echo "${PERSON1}: How are you?"
+echo "${PERSON2} Am doing good, How about you ${PERSON1}?"
+echo "${PERSON1}: Am doing great, Thanks for asking ${PERSON2}"
