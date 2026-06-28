4780cfb70f3a359177d282eb85b23c19b4be36a2
Thoyaz <thoyazthoyazpc@gmail.com>
2026-06-28 22:56:32 +0530

    Added log file deleting script

--- delete-old-logs-method-02.sh
+++ delete-old-logs-method-02.sh
@@ -9,18 +9,18 @@ LOGS_DIR=/home/tz/delete_logs
 SCRIPT_LOGS="$LOGS_DIR/$(basename "$0").log"
 
 if [ ! $LOGS_DIR ]; then
-    echo " $R $LOGS_DIR Log folder does not exists $N"
+    echo -e " $R $LOGS_DIR Log folder does not exists $N"
     exit 1
 fi
 
 FILES_TO_DELETE=$(find $LOGS_DIR -name '*.log' -type f -mtime +14)
 
 if [ -z "$FILES_TO_DELETE" ]; then
-    echo "$R Logs files olderthan 14 days was not exist $N"
+    echo -e "$R Logs files olderthan 14 days was not exist $N"
     else
         while IFS= read -r filepath; do
             echo "Deleting file...$filepath"
             rm -f $filepath
-            echo "$G Deleted file...$filepath $N"
+            echo -e "$G Deleted file...$filepath $N"
         done <<< $FILES_TO_DELETE
 fi
